3ffc135080690f01a87aa4945c87162147b753c6
blogginformore <49066559+aalexlpez@users.noreply.github.com>
2025-02-27 10:59:32 +0100

    fix - actualización

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -1,30 +1,28 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+import { NextResponse } from 'next/server'
+import { Resend } from 'resend'
+import { template } from '../../../utils/email-template'
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST(request: Request) {
-    const { name, email, message } = await request.json();
-    console.log(name, email, message)
+export async function POST (request: Request) {
+  const { name, email, message } = await request.json()
+  console.log(name, email, message)
 
   try {
+    const emailTemplate = template(name, message)
+
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: 'onboarding@resend.dev',
       to: email,
-      subject: "Bienvenido a Tu Viaje Espiritual",
-      html: `
-        <h1>Hola ${name},</h1>
-        <p>Gracias por unirte a nuestro camino espiritual.</p>
-        <p>Tu mensaje: <strong>${message}</strong></p>
-        <p>Recibirás meditaciones guiadas y más. ¡Pronto te contactaremos!</p>
-      `,
-    });
-    return NextResponse.json({ message: "Correo enviado con éxito" });
+      subject: 'Bienvenido a Tu Viaje Espiritual',
+      html: emailTemplate
+    })
+    return NextResponse.json({ message: 'Correo enviado con éxito' })
   } catch (error) {
-    console.error("Error enviando correo:", error);
+    console.error('Error enviando correo:', error)
     return NextResponse.json(
-      { message: "Error al enviar el correo" },
+      { message: 'Error al enviar el correo' },
       { status: 500 }
-    );
+    )
   }
 }
