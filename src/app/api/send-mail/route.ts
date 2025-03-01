c0bcb577cf30b9129fd5d0b0d33996fec6f11456
blogginformore <49066559+aalexlpez@users.noreply.github.com>
2025-03-02 00:54:34 +0100

    fix nombre de correo

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
     const emailTemplate = template(name)
 
     await resend.emails.send({
-      from: 'Spirituale <spirituale@spiritualelife.com>',
+      from: 'Team Spirituale <info@spiritualelife.com>',
       to: email,
       subject: 'Bienvenido a Tu Viaje Espiritual',
       html: emailTemplate
