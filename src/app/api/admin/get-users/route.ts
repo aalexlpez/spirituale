be4a302417f1442ab427bdcaea7d812bdbb9c5bf
blogginformore <49066559+aalexlpez@users.noreply.github.com>
2025-03-02 17:19:34 +0100

    Update route.ts

--- src/app/api/admin/get-users/route.ts
+++ src/app/api/admin/get-users/route.ts
@@ -1,16 +1,14 @@
 import { NextResponse } from 'next/server';
 import { db } from '../../../../../lib/utils/firebaseConfig';
-import { collection, getDocs } from 'firebase/firestore';
-
-// const resend = new Resend(process.env.RESEND_API_KEY)
+import { collection, getDocs, QueryDocumentSnapshot } from 'firebase/firestore';
 
 export async function GET() {
   try {
     const usersCollection = collection(db, 'potencialCliente');
     const usersSnapshot = await getDocs(usersCollection);
-    const usersList = usersSnapshot.docs.map((doc: any) => doc.data());
+    const usersList = usersSnapshot.docs.map((doc: QueryDocumentSnapshot) => doc.data());
     return NextResponse.json(usersList);
-  } catch (error) {
+  } catch {
     return NextResponse.json({ error: 'Error consultando usuarios' }, { status: 500 });
   }
 }
